20a6563d42328c9722e20eb98bf59b6e236f01d3
sarajac1 <sarajac@hotmail.com>
2024-10-08 11:11:53 +0200

    Successfully created new SQLite db.

--- backend/app.py
+++ backend/app.py
@@ -4,10 +4,16 @@ import sqlite3
 # Initialiserar Flask app 
 app = Flask(__name__)
 
+def connect_db ():
+  return sqlite3.connect('pantry.db')
+
 # Funktion för att hantera routes, json format  
 def home():
   return jsonify({"message": "Welcome to PantryDropper."})
 
-# Startar Flask med debugging 
+def add_item():
+  
+
+# Om skriptet körs direkt så startas Flask med debugging  
 if __name__ == '__main__':
   app.run(debug=True)
--- backend/setup_db.py
+++ backend/setup_db.py
@@ -1,13 +1,15 @@
 import sqlite3
+import os 
 
 # Funktion som skapar SQLite databas
 def create_db():
+  print("Current working directory:", os.getcwd())
   connection = sqlite3.connect('pantry.db')
   cursor = connection.cursor()
 
   cursor.execute('''
     CREATE TABLE IF NOT EXISTS inventory (
-        id INTEGER PRIMARY KEY,
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
         item_name TEXT NOT NULL,
         quantity INTEGER NOT NULL
     )
@@ -17,6 +19,7 @@ def create_db():
   connection.commit()
   connection.close()
 
-  # Programmet körs endast om script exekverats direkt 
-  if __name__ == '__main__':
-    create_db()
+  
+# Programmet körs endast om script exekverats direkt 
+if __name__ == '__main__':
+  create_db()
